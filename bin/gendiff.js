fb61549f25e092af179137e885136f26deeafb71
IvanLi <litvinovivanvadimovich@gmail.com>
2024-06-13 09:52:17 +0300

    gendiff.js file added '.argument and .options'

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description(' Compares two configuration files and shows a difference')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filePath1> <filePath2>')
+  .option('-f, --format<type>', 'choose format');
 
 program.parse();
