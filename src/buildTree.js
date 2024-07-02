1f1ff902cc6a0953e5a190bc2177564dbfa19f63
IvanLi <litvinovivanvadimovich@gmail.com>
2024-07-02 17:26:43 +0300

    change buildTree

--- src/buildTree.js
+++ src/buildTree.js
@@ -5,38 +5,29 @@ const getKey = (data1, data2) => _.union(Object.keys(data1), Object.keys(data2))
 const buildTree = (data1, data2) => {
   const keys = getKey(data1, data2);
 
-  return keys.reduce((acc, key) => {
-    let diffEntry;
-
+  return keys.map((key) => {
     switch (true) {
       case !_.has(data2, key):
-        diffEntry = { key, type: 'removed', value: data1[key] };
-        break;
+        return { key, type: 'removed', value: data1[key] };
       case !_.has(data1, key):
-        diffEntry = { key, type: 'added', value: data2[key] };
-        break;
+        return { key, type: 'added', value: data2[key] };
       case _.isObject(data1[key]) && _.isObject(data2[key]):
-        diffEntry = {
+        return {
           key,
           type: 'nested',
           children: buildTree(data1[key], data2[key]),
         };
-        break;
       case !_.isEqual(data1[key], data2[key]):
-        diffEntry = {
+        return {
           key,
           type: 'updated',
           oldValue: data1[key],
           newValue: data2[key],
         };
-        break;
       default:
-        diffEntry = { key, type: 'unchanged', value: data1[key] };
+        return { key, type: 'unchanged', value: data1[key] };
     }
-
-    acc.push(diffEntry);
-    return acc;
-  }, []);
+  });
 };
 
 export default buildTree;
