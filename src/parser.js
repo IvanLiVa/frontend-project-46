e9369a1c2c68e2ec967200bb5fc7abaeeb88cd83
IvanLi <litvinovivanvadimovich@gmail.com>
2024-07-02 15:07:45 +0300

    exept let in parseData

--- src/parser.js
+++ src/parser.js
@@ -1,19 +1,15 @@
 import yaml from 'js-yaml';
 
 const parseData = (data, format) => {
-  let parsedData;
   switch (format) {
     case '.json':
-      parsedData = JSON.parse(data);
-      break;
+      return JSON.parse(data);
     case '.yml':
     case '.yaml':
-      parsedData = yaml.load(data);
-      break;
+      return yaml.load(data);
     default:
       throw new Error(`Unknown format: ${format}`);
   }
-  return parsedData;
 };
 
 export default parseData;
